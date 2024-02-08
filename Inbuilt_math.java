ddc5cb31928f6532ec5e5f90ae90e6b44e67f9d6
rajshekhar <01fe22bca009@kletech.ac.in>
2024-02-08 22:07:16 +0530

    program using math library

---
+++ Inbuilt_math.java
@@ -0,0 +1,15 @@
+public class Inbuilt_math {
+    public static void main(String args[])
+    {
+        System.out.println("Maximum of a number = "+Math.max(6,3));
+        System.out.println("MInimum of a number = "+Math.min(6,3));
+        System.out.println("Square root of a number = "+Math.sqrt(6));
+        System.out.println("Maximum of a number = "+Math.cbrt(6));
+        System.out.println("Maximum of a number = "+Math.pow(6,3));
+        System.out.println("Maximum of a number = "+Math.random());
+
+
+
+    }
+    
+}

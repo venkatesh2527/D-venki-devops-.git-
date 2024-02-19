02be0a3eb5fce4629b4211cf7d3b94a874dfde82
venkatesh2527 <157194431+venkatesh2527@users.noreply.github.com>
2024-02-19 14:30:14 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -1,27 +1,27 @@
  
-public class SimpleCalculator {
-    public static void main(String[] args) {
-        int num1 = 10; // Change these values as needed
-        int num2 = 5;
-       
-        // Addition
-        int sum = num1 + num2;
-        System.out.println("Sum: " + sum);
-       
-        // Subtraction
-        int difference = num1 - num2;
-        System.out.println("Difference: " + difference);
-       
-        // Multiplication
-        int product = num1 * num2;
-        System.out.println("Product: " + product);
-       
-        // Division
-        if (num2 != 0) {
-            double quotient = (double) num1 / num2;
-            System.out.println("Quotient: " + quotient);
-        } else {
-            System.out.println("Cannot divide by zero");
-        }
-    }
+public class cal{
+  public static void main(String[] args) {
+      int num1 = 10; // Change these values as needed
+      int num2 = 5;
+     
+     
+      int sum = num1 + num2;
+      System.out.println("Sum: " + sum);
+     
+    
+      int difference = num1 - num2;
+      System.out.println("Difference: " + difference);
+     
+      
+      int product = num1 * num2;
+      System.out.println("Product: " + product);
+     
+    
+      if (num2 != 0) {
+          double quotient = (double) num1 / num2;
+          System.out.println("Quotient: " + quotient);
+      } else {
+          System.out.println("Cannot divide by zero");
+      }
+  }
 }
